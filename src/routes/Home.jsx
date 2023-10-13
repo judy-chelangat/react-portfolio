1cc35a133a1bb6688e8ccdf5baccbd1cf5d3b64e
judy-chelangat <judysigilai75@gmail.com>
2023-10-13 21:53:52 +0300

    imported heroimage into home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar  from '../components/Navbar'
+import HeroImage from '../components/HeroImage'
 function Home() {
   return (
     <div>
       <Navbar/>
+      <HeroImage/> 
     </div>
   )
 }
