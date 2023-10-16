9d423e0cd9a570e72f8313d4649a91c0beaf907b
judy-chelangat <judysigilai75@gmail.com>
2023-10-16 10:34:57 +0300

    added heroimage component

---
+++ src/components/HeroImage2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './HeroImage2.css'
+
+function HeroImage2() {
+  return (
+    <div className='hero-img'>
+      <div className='heading'>
+       <h1></h1>
+       <p></p>
+      </div>
+    </div>
+  )
+}
+
+export default HeroImage2
