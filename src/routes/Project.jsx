69e8cb6a570b9a1647e6a83ce74ee2ff71de1730
judy-chelangat <judysigilai75@gmail.com>
2023-10-16 10:35:41 +0300

    rendered the navbar and footer

--- src/routes/Project.jsx
+++ src/routes/Project.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import HeroImage2 from '../components/HeroImage2'
 
 function Project() {
   return (
     <div>
-      
+      <Navbar/>
+      <HeroImage2/>
+      <Footer/>
     </div>
   )
 }
