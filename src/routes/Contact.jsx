2cf390f3d82d7d11e4d5997f3ecb73c7d116f8d7
judy-chelangat <judysigilai75@gmail.com>
2023-10-16 10:35:28 +0300

    rendered the navbar and footer

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 function Contact() {
   return (
     <div>
-      
+      <Navbar/>
+      <Footer/>
     </div>
   )
 }
